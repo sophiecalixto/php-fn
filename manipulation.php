dee516552977742ac4d34ccee5065f3454324e34
Sophie Calixto <sophiecalixto2004@gmail.com>
2023-12-01 12:55:00 -0300

    working with data manipulation (short closures)

--- manipulation.php
+++ manipulation.php
@@ -3,23 +3,13 @@
 $data = require 'data.php';
 
 // Create a new array with the country names only (no medals)
-$countryNames = array_map(function ($country) {
-    return $country['country'];
-}, $data);
+$countryNames = array_map(fn (array $country): string => $country['country'], $data);
 // Create a new array with the country names in uppercase
-$countryNamesUpperCase = array_map(function ($country) {
-    return strtoupper($country['country']);
-}, $data);
+$countryNamesUpperCase = array_map(fn (array $country): string => strtoupper($country['country']), $data);
 //Create a new array with countries that have the letter A
-$countriesWithLetterA = array_filter($data, function($country) {
-    return str_contains($country['country'], 'A');
-});
+$countriesWithLetterA = array_filter($data, fn($country) : string => str_contains($country['country'], 'A'));
 //Create a new array with the value of all medal
-$allMedals = array_map(function ($country) {
-    return array_sum($country['medals']);
-}, $data);
-$allMedalsQuantity = array_reduce($allMedals, function($countable, $actualCount) {
-    return $countable + $actualCount;
-}, 0);
+$allMedals = array_map(fn ($country) : float|int => array_sum($country['medals']), $data);
+$allMedalsQuantity = array_reduce($allMedals, fn($countable, $actualCount) : int => $countable + $actualCount, 0);
 
 var_dump($allMedalsQuantity);
